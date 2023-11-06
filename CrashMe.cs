6bafff8cf9a1c34b6349fe63a4ec32eea49a1f1d
Mikkel <admin@integration-it.com>
2023-11-06 14:05:10 +0100

    Added functions

---
+++ CrashMe.cs
@@ -0,0 +1,27 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace teknologisk_functionapp
+{
+    public class CrashMe
+    {
+        private readonly ILogger _logger;
+
+        public CrashMe(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<CrashMe>();
+        }
+
+        [Function("CrashMe")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            throw new Exception($"I crashed the function at {DateTime.UtcNow}");
+
+            
+        }
+    }
+}
